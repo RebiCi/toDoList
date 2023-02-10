050ee30a0f4de1d25f116deabc4895c17286f50f
Gerardo Kokoshari <zargonblack@gmail.com>
2023-02-10 14:58:29 +0100

    update

--- toDoList.js
+++ toDoList.js
@@ -9,30 +9,28 @@ let todoList = [];
 
 button.addEventListener("click", (e) => {
   //qui tutto quello che succede dopo il click -> aggiungo gli elementi alla lista
-
   const main = document.createElement("div");
-
   const task = document.createElement("p"); // inserisco nell html un <p>
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("delete");
-  const check = document.createElement("input");
-  check.setAttribute("type", "checkbox");
+
   deleteButton.innerText = "cancella";
   const content = document.createTextNode(input.value);
-  console.log(input.value);
-  todoList.push(content.nodeValue);
+  console.log("input: ", input.value);
+  todoList.push(content.nodeValue); // inseriamo il task nell'array 
   input.value = "";
   console.log(todoList);
   e.preventDefault(); //evita l'azzermento prima del caricamento
-  task.appendChild(content);
-  main.appendChild(task);
-  main.appendChild(deleteButton);
-  main.appendChild(check);
+  task.appendChild(content); // <p> content </p>
+  main.appendChild(task); // <div> <p> content </p> <div>
+  main.appendChild(deleteButton); // <div> <p> content </p> <button> cancella </button></div>
   div.appendChild(main);
 
-  var current_tasks = document.querySelectorAll(".delete");
-  for (var i = 0; i < current_tasks.length; i++) {
-    current_tasks[i].onclick = function () {
+  var current_tasks = document.querySelectorAll(".delete"); // cicla tutti 
+  for (var i = 0; i < current_tasks.length; i++) // cicla tutti gli elementi con classe "delete"
+  {
+    current_tasks[i].onclick = function () // al click viene "nascosto" il task
+    {
       this.parentNode.remove();
       console.log(this.parentNode);
     };
